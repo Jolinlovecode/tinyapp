a7d985d025910c3b5dc5a327b51477f758922d04
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-01-26 22:10:56 -0700

    express ejs cookie

--- express_server.js
+++ express_server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
+const cookieparser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieparser());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-
 };
 
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+console.log(req.cookies);
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -43,11 +44,29 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
+
+app.post("/login", (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/urls');
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase/* What goes here? */ };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
